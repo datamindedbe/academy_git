7499083a760281ad39e75a627fef3f9c76106110
nathan.derave <nathan.derave@dataminded.be>
2021-08-05 09:12:50 +0200

    Debug exercise_6 commit 1 cp command

--- content/resources/exercise_6/bootstrap.sh
+++ content/resources/exercise_6/bootstrap.sh
@@ -8,7 +8,7 @@ RESOURCES_EX6="../resources/exercise_6"
 
 git init
 
-cp $RESOURCES_EX6/commit1/* .
+cp -r $RESOURCES_EX6/commit1/* .
 git add .
 git commit -am "Initial commit. v1 of the server."
 
