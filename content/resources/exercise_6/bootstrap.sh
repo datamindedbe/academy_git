4ddd7bceeb5190644a1010ff8e3a374a99c351ec
nathan.derave <nathan.derave@dataminded.be>
2021-08-05 09:06:24 +0200

    Made exercise_6 + renamed exercice to exerciSe

---
+++ content/resources/exercise_6/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT="$(dirname "$0")/../.."
+cd "$ROOT/exercise_6"
+
+RESOURCES_EX2="../resources/exercise_2"
+RESOURCES_EX6="../resources/exercise_6"
+
+git init
+
+cp $RESOURCES_EX6/commit1/* .
+git add .
+git commit -am "Initial commit. v1 of the server."
+
+cp $RESOURCES_EX6/README.md .
+git add .
+git commit -am "Added the README for students"
