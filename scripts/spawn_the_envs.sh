a40e6d46428f25284b840617ec5332a8dde3de7d
nathan.derave <nathan.derave@dataminded.be>
2022-10-04 23:03:37 +0200

    Cleaned legacy instructions for self-hosting

--- scripts/spawn_the_envs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "What's the port start range ? : " start_port
-read -p "What's the number of environment to deploy ? : " n_envs
-
-for ((i=start_port; i<start_port+n_envs; i++)); do
-	sudo docker run --rm -d -p $i:8443 --hostname git-introduction-training git-introduction-training:latest
-done
